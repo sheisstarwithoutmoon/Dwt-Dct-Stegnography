0cbec798de667273a749f3e92171b258ee220d2c
Vanya <iamvanyaawasthi@gmail.com>
2026-03-27 12:09:32 +0530

    fixed dwt

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+import argparse
 from src.utils.image_utils import load_image, save_image
+from src.dwt.dwt_embed import embed_dwt
+from src.dwt.dwt_extract import extract_dwt
 from src.dct.dct_embed import embed_dct
 from src.dct.dct_extract import extract_dct
 from src.utils.metrics import psnr
 
-image = load_image("data/input/images/input.png")
 
-secret = "Hello Vanya"
+def main():
+	parser = argparse.ArgumentParser(description="Embed and extract using DWT or DCT")
+	parser.add_argument('--method', choices=['dwt', 'dct'], default='dwt', help='Steganography method')
+	parser.add_argument('--input', default='data/input/images/input.png', help='Input image path')
+	parser.add_argument('--output', default='data/output/stego.png', help='Output stego image path')
+	args = parser.parse_args()
 
-stego = embed_dct(image.copy(), secret)
+	image = load_image(args.input)
+	secret = "Hello, this is a secret message hidden in the image using DCT steganography!"
 
-save_image("data/output/stego.png", stego)
+	if args.method == 'dwt':
+		stego = embed_dwt(image.copy(), secret)
+		decoded = extract_dwt(stego)
+	else:
+		stego = embed_dct(image.copy(), secret)
+		decoded = extract_dct(stego)
 
-decoded = extract_dct(stego)
-print("Extracted:", decoded)
+	save_image(args.output, stego)
 
-print("PSNR:", psnr(image, stego))
+	print("Extracted:", decoded)
+	print("PSNR:", psnr(image, stego))
+
+
+if __name__ == '__main__':
+	main()
 
